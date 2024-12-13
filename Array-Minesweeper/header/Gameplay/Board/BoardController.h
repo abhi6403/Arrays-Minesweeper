e5b5c8a7e2a628b1f3af23734d2334ab94cdc2ef
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-12-13 10:59:05 +0530

    Creating Board Controller

---
+++ Array-Minesweeper/header/Gameplay/Board/BoardController.h
@@ -0,0 +1,30 @@
+#pragma once
+namespace Gameplay
+{
+	namespace Board
+	{
+		class BoardController
+		{
+		private:
+		
+			void createBoard();
+			void deleteBoard();
+			void destroy();
+			
+
+		public:
+			static const int number_of_rows_ = 9;
+			static const int number_of_colums = 9;
+			static const int mines_count = 8;
+
+			BoardController();
+			~BoardController();
+
+			void initialize();
+			void update();
+			void render();
+			void reset();
+
+		};
+	}
+}
